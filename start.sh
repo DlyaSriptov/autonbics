341e8edfab9cfe19f4168d6c2177893fe1d44af6
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-11 13:53:56 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,21 +8,21 @@ set_check=Y
 for ((;;))
 do
     echo "-------------------------------------------"
-    echo "Пожалуйста, ответьте на несколько вопросов (Please answer questions):"
+    echo "Пожалуйста, ответьте на несколько вопросов (Otvet'te na voprosy):"
     echo "-------------------------------------------"
-    echo "Настроить сеть (Set up network) [Y/N]?"
+    echo "Настроить сеть (Nastroit' seti) [Y/N]?"
     read netconf
             
-    echo "Установить Jitsi (Install Jitsi) [Y/N]?"
+    echo "Установить Jitsi (Ustanovit' Jitsi) [Y/N]?"
     read set_jitsi
     
-    echo "Установить NBICS (Install NBICS) [Y/N]?"
+    echo "Установить NBICS (Ustanovit' NBICS) [Y/N]?"
     read set_nbics
     
     echo "==================================="
-    echo "Настройка сети - " $netconf
-    echo "Установка Jitsi - " $set_jitsi
-    echo "Установка NBICS - " $set_nbics
+    echo "Настройка сети (Nastrojka seti) - " $netconf
+    echo "Установка Jitsi (Ustanovka Jitsi) - " $set_jitsi
+    echo "Установка NBICS (Ustanovka NBICS) - " $set_nbics
     
     echo "==================================="
     echo "                                   "
