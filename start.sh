78dc67055cde6c65b518756538f7033af8ca5437
DlyaScriptov <136061715+DlyaScriptov@users.noreply.github.com>
2023-06-13 11:43:15 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -63,11 +63,18 @@ echo -n > ./autonbics/questions.txt
 cp ./autonbics/files/template_questions.txt ./autonbics/questions.txt
 
 # Проверка файла с настройками (если не пустой - просить брать настройки оттуда)
-checkFileWithPreset=$(wc -m ./autonbics/files/preset_questions.txt | grep -o '[0-9]*')
-if [[ $checkFileWithPreset > 0 ]]
-    then
-        echo "Файл не пустой. Он содержит" $testvar "символов."
-    fi
+## checkFileWithPreset=$(wc -m ./autonbics/files/preset_questions.txt | grep -o '[0-9]*')
+## questPreset=Y
+## if [[ $checkFileWithPreset > 0 ]]
+## then
+    ## echo "У вас сохранены предыдущие настройки. Использовать их? [Y/N]"
+    ## echo "U vas sohraneny predydushchie nastrojki. Ispol'zovat' ih? [Y/N]"
+    ## read questPreset
+    ## if [[ $questPreset = Y ]]
+    ## then
+        
+    ## fi
+## fi
 
 # Функции для заполнения опросника questions.txt
 # В опроснике - заготовленные ответы для настройки программ
